329b12dc051b2c68727872dd9cef43e4817fa33b
Louis Maddox <louismmx@gmail.com>
2023-07-26 20:30:29 +0100

    HTML hello

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(request: Request):
+    message = "Hello World"
+    return templates.TemplateResponse("index.html", {"request": request, "message": message})
