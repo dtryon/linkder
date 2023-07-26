ac82757ba401b8d582b713a2ed6c7e3559ff212e
adam3TN <140652120+adam3TN@users.noreply.github.com>
2023-07-26 21:07:42 +0100

    Add files via upload

---
+++ app/profiles/profiles.py
@@ -0,0 +1,61 @@
+[{
+  "name": "Leopald Lael",
+  "distance": 2200,
+  "qualifications": "Mustang",
+  "pemployers: 3,
+  "numEmojis": 2,
+  "SkilsTags": 5,
+  "BuzTags":3
+},
+
+ {
+  "name": "Bryn Hanna",
+  "distance": 15,
+  "qualifications": "Mustang",
+  "pemployers: 3,
+  "numEmojis": 4,
+  "SkilsTags": 3,
+  "BuzTags": 5
+},
+
+{
+  "name": "Matfey Pomare",
+  "distance": 15,
+  "qualifications": "Mustang",
+  "pemployers: 4,
+  "numEmojis": 2,
+  "SkilsTags": 7,
+  "BuzTags": 15
+},   
+
+{
+  "name": "John Husam",
+  "distance": 15,
+  "qualifications": "Mustang",
+  "pemployers: 3,
+  "numEmojis": 4,
+  "SkilsTags": 5,
+  "BuzTags": 14
+},   
+
+{
+  "name": "Megan Lucrezia",
+  "distance": 15,
+  "qualifications": "Mustang",
+  "pemployers: 3,
+  "numEmojis": 4,
+  "SkilsTags": 5,
+  "BuzTags": 18
+},   
+
+{
+  "name": "Tamsin Chi",
+  "distance": 15,
+  "qualifications": "Mustang",
+  "pemployers: 3,
+  "numEmojis": 19,
+  "SkilsTags": 12,
+  "BuzTags": 11
+}   
+]
+
